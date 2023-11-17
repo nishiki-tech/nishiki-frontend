ad46efcdd9f1a376cbba22d33e9e99a8657baeda
kanta1207 <kan0712livepool@gmail.com>
2023-11-17 15:21:00 -0800

    fix : replace icons with the ones in wireframe

--- src/components/ui/icons/links/GroupsIconLink.tsx
+++ src/components/ui/icons/links/GroupsIconLink.tsx
@@ -1,7 +1,6 @@
 import { mainRouteConfig, MainRouteName } from '@/const/site/mainRouteConfig';
 import { cn } from '@/lib/tailwind/utils';
 
-import { Home } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
@@ -19,7 +18,18 @@ const GroupsIconLink: React.FC<Props> = ({ isSelected, className }) => {
       href={mainRouteConfig[name]}
       className="inline-flex flex-col items-center justify-center px-5"
     >
-      <Home className={cn(isSelected ? 'stroke-primary' : 'stroke-secondary', className)} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        className={cn(isSelected ? 'stroke-primary' : 'stroke-secondary', className)}
+      >
+        <g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+          <path d="M6.133 21C4.955 21 4 20.02 4 18.81v-8.802c0-.665.295-1.295.8-1.71l5.867-4.818a2.09 2.09 0 0 1 2.666 0l5.866 4.818c.506.415.801 1.045.801 1.71v8.802c0 1.21-.955 2.19-2.133 2.19H6.133Z" />
+          <path d="M9 16c.85.63 1.885 1 3 1s2.15-.37 3-1m-5.5-4.5V11m5 .5V11" />
+        </g>
+      </svg>
       <span className={cn('text-sm', isSelected ? 'text-primary' : 'text-secondary')}>{name}</span>
     </Link>
   );
