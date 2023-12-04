e6021f590f9b7fe1f3f398d4ca972e48fc3296a9
Yusuke Ito <99148565+uskayyyyy@users.noreply.github.com>
2023-12-03 23:31:40 -0800

    build: upgrade Next.js to v14 (#49)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,6 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ],
   icons: {
     // TODO add actual icons
     icon: '/favicon.ico',
