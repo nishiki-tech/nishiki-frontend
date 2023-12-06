6e92d996ce6b5f9b40d53969a9a4e616bfe1fff3
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-06 00:40:08 -0800

    feat: apply theme colors and sizes to button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,27 +5,23 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'text-base min-w-[70px] rounded inline-flex items-center justify-center gap-2.5',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        primary: 'bg-primary text-white hover:bg-primary-dark',
+        error: 'bg-danger text-white hover:bg-danger-dark',
+        cancel: 'bg-transparent text-black hover:bg-gray-light',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        sm: 'h-8 px-7',
+        md: 'h-10 px-12',
+        lg: 'h-12 px-12',
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      variant: 'primary',
+      size: 'md',
     },
   },
 );
