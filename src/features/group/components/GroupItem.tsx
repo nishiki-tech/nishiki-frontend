b829528cbf97a614e654a490a3dba8bc47c2422c
kotaaaa <kota.k.1132.pda@gmail.com>
2023-12-10 12:52:30 -0800

    fix: place to put Link tag

--- src/features/group/components/GroupItem.tsx
+++ src/features/group/components/GroupItem.tsx
@@ -3,6 +3,8 @@ import { IUser } from '@/features/group/types/definition';
 import { fetchContainerList } from '@/lib/api/data';
 import { fetchUserList } from '@/lib/api/data';
 
+import Link from 'next/link';
+
 import { IGroup } from '../types/definition';
 import ContainersCount from './ContainersCount';
 import UserCount from './UserCount';
@@ -10,11 +12,11 @@ export default async function GroupItem({ group }: { group: IGroup }) {
   const containers: IContainer[] = await fetchContainerList(group.groupId);
   const users: IUser[] = await fetchUserList(group.groupId);
   return (
-    <>
+    <Link href={`/groups/${group.groupId}`} key={group.groupId}>
       <span>{group.groupId} </span>
       <span>{group.groupName} </span>
       <ContainersCount containerCount={containers.length} />
       <UserCount userCount={users.length} />
-    </>
+    </Link>
   );
 }
--- src/features/group/components/GroupList.tsx
+++ src/features/group/components/GroupList.tsx
@@ -1,14 +1,10 @@
-import Link from 'next/link';
-
 import { IGroup } from '../types/definition';
 import GroupItem from './GroupItem';
 export default async function GroupList({ groups }: { groups: IGroup[] }) {
   return (
     <>
       {groups.map((group) => (
-        <Link href={`/groups/${group.groupId}`} key={group.groupId}>
-          <GroupItem key={group.groupId} group={group} />
-        </Link>
+        <GroupItem key={group.groupId} group={group} />
       ))}
     </>
   );
