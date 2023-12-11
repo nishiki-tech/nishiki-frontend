c078103d5dafcec95676339f82a1bc02a58b0d78
Yusuke Ito <99148565+uskayyyyy@users.noreply.github.com>
2023-12-11 12:25:04 -0800

    Dialog component (#79)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,16 @@
 export { BottomTabIconLink } from './BottomTabIconLink';
 export { Button, buttonVariants } from './Button';
+export {
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from './Dialog';
 export { Icon } from './Icon';
