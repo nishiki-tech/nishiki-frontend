2168b4d8bbc2c393efaa7f6a626036d890b26ff0
kotaaaa <kota.k.1132.pda@gmail.com>
2023-12-13 09:57:00 -0800

    feat: fix lint format

--- src/features/groups/components/ContainerList.tsx
+++ src/features/groups/components/ContainerList.tsx
@@ -3,7 +3,6 @@ import { fetchContainerList } from '@/lib/api/data';
 
 import { IContainer } from '../types/definition';
 
-
 export const ContainerList = async ({ id }: { id: string }) => {
   const containers: IContainer[] = await fetchContainerList(id);
   return (
