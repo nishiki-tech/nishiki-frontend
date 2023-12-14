41cea7fbdd83155c613c011540fbfac8fdb72d8f
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-14 14:02:26 -0800

    feat: create SelectionDrawerButton component

---
+++ src/components/ui/SelectionDrawer/SelectionDrawerButton.tsx
@@ -0,0 +1,65 @@
+import { cn } from '@/lib/tailwind/utils';
+
+import { Slot } from '@radix-ui/react-slot';
+import {
+  ButtonHTMLAttributes,
+  FC,
+  forwardRef,
+  ReactElement,
+  ReactNode,
+  SVGAttributes,
+} from 'react';
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+  children?: ReactNode;
+}
+
+const SelectionDrawerButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, asChild = false, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp className={cn('h-12 flex items-center w-full text-lg', className)} ref={ref} {...props}>
+        {children}
+      </Comp>
+    );
+  },
+);
+SelectionDrawerButton.displayName = 'SelectionDrawerButton';
+
+interface ISelectionDrawerIconProps {
+  children: ReactElement<FC<SVGAttributes<SVGElement>>>;
+  className?: string;
+}
+
+const SelectionDrawerButtonIcon = forwardRef<HTMLDivElement, ISelectionDrawerIconProps>(
+  ({ children, className }, ref) => {
+    return (
+      <div
+        className={cn('h-full aspect-square mx-0.5 flex items-center justify-center', className)}
+        ref={ref}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+SelectionDrawerButtonIcon.displayName = 'SelectionDrawerButtonIcon';
+
+interface ISelectionDrawerLabelProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const SelectionDrawerButtonLabel = forwardRef<HTMLSpanElement, ISelectionDrawerLabelProps>(
+  ({ children, className }, ref) => {
+    return (
+      <span className={cn(className)} ref={ref}>
+        {children}
+      </span>
+    );
+  },
+);
+SelectionDrawerButtonLabel.displayName = 'SelectionDrawerButtonLabel';
+
+export { SelectionDrawerButton, SelectionDrawerButtonIcon, SelectionDrawerButtonLabel };
