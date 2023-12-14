423e4928f5faa60a4e3b5cbb2c34b0718d14a621
Yusuke Ito <99148565+uskayyyyy@users.noreply.github.com>
2023-12-13 18:52:06 -0800

    Created a reusable "Drawer" component (#89)

---
+++ src/components/ui/DrawerUsageExample.tsx
@@ -0,0 +1,75 @@
+/**
+ * This file is used as an example for the Drawer component.
+ * Once you're done with the example, you can delete this file.
+ */
+'use client';
+
+import {
+  Button,
+  DrawerBody,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerRoot,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui';
+
+import { useState } from 'react';
+
+export const DrawerUsageExample = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDelete = () => {
+    alert('Deleted!');
+    setIsDrawerOpen(false);
+  };
+
+  return (
+    <DrawerRoot open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
+      <DrawerTrigger asChild>
+        <Button>Open</Button>
+      </DrawerTrigger>
+      <DrawerContent side="bottom">
+        <DrawerHeader>
+          <DrawerTitle>Delete Container</DrawerTitle>
+        </DrawerHeader>
+        <DrawerBody>
+          <ol>
+            <li>1. Are you sure you want to delete?</li>
+            <li>2. Are you sure you want to delete?</li>
+            <li>3. Are you sure you want to delete?</li>
+            <li>4. Are you sure you want to delete?</li>
+            <li>5. Are you sure you want to delete?</li>
+            <li>6. Are you sure you want to delete?</li>
+            <li>7. Are you sure you want to delete?</li>
+            <li>8. Are you sure you want to delete?</li>
+            <li>9. Are you sure you want to delete?</li>
+            <li>10. Are you sure you want to delete?</li>
+            <li>11. Are you sure you want to delete?</li>
+            <li>12. Are you sure you want to delete?</li>
+            <li>13. Are you sure you want to delete?</li>
+            <li>14. Are you sure you want to delete?</li>
+            <li>15. Are you sure you want to delete?</li>
+            <li>16. Are you sure you want to delete?</li>
+            <li>17. Are you sure you want to delete?</li>
+            <li>18. Are you sure you want to delete?</li>
+            <li>19. Are you sure you want to delete?</li>
+            <li>20. Are you sure you want to delete?</li>
+          </ol>
+        </DrawerBody>
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant="cancel" size="sm">
+              Cancel
+            </Button>
+          </DrawerClose>
+          <Button variant="error" size="sm" onClick={handleDelete}>
+            Delete
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </DrawerRoot>
+  );
+};
