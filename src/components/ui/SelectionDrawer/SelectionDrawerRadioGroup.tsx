d38374943b558b4cbe08697b6fdc01a4d559a334
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-14 14:08:03 -0800

    feat: create SelectionDrawerRadioGroup component

---
+++ src/components/ui/SelectionDrawer/SelectionDrawerRadioGroup.tsx
@@ -0,0 +1,39 @@
+import { cn } from '@/lib/tailwind/utils';
+
+import {
+  Indicator as PrimitiveIndicator,
+  Item as PrimitiveItem,
+  Root as PrimitiveRoot,
+} from '@radix-ui/react-radio-group';
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
+
+const SelectionDrawerRadioGroup = forwardRef<
+  ElementRef<typeof PrimitiveRoot>,
+  ComponentPropsWithoutRef<typeof PrimitiveRoot>
+>(({ className, ...props }, ref) => {
+  return <PrimitiveRoot className={cn('', className)} {...props} ref={ref} />;
+});
+SelectionDrawerRadioGroup.displayName = PrimitiveRoot.displayName;
+
+const SelectionDrawerRadioGroupItem = forwardRef<
+  ElementRef<typeof PrimitiveItem>,
+  ComponentPropsWithoutRef<typeof PrimitiveItem>
+>(({ className, ...props }, ref) => {
+  return (
+    <PrimitiveItem
+      ref={ref}
+      className={cn(
+        'aspect-square h-6 w-6 rounded-full border border-primary text-primary',
+        className,
+      )}
+      {...props}
+    >
+      <PrimitiveIndicator className="flex items-center justify-center">
+        <div className="w-3 h-3 bg-primary rounded-full"></div>
+      </PrimitiveIndicator>
+    </PrimitiveItem>
+  );
+});
+SelectionDrawerRadioGroupItem.displayName = PrimitiveItem.displayName;
+
+export { SelectionDrawerRadioGroup, SelectionDrawerRadioGroupItem };
