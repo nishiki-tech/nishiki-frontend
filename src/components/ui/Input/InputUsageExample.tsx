4b65978dcbea23be9aa62e08f68d18fa1a7f4223
kanta1207 <kan0712livepool@gmail.com>
2023-12-16 00:11:22 -0800

    test : add usage sample

---
+++ src/components/ui/Input/InputUsageExample.tsx
@@ -0,0 +1,19 @@
+/**
+ * This file is used as an example for the Dialog component.
+ * Once you're done with the example, you can delete this file.
+ */
+
+import { Input, SearchInput } from '..';
+
+export function InputWithLabelUsageExample() {
+  return (
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Input type="text" id="groupName" placeholder="Group name" />
+      <SearchInput placeholder="Search Foods" />
+      <div className="w-1/2">
+        {/* When it's type number, it allows input like 01111 */}
+        <Input type="number" id="quantity" placeholder="Quantity" />
+      </div>
+    </div>
+  );
+}
