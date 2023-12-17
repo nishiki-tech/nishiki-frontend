a1b4875d29a5bcef3c3724679b988effed75a7dc
kanta1207 <kan0712livepool@gmail.com>
2023-12-17 00:00:54 -0800

    fix : fix SearchInput to make container clickable

--- src/components/ui/Input/SearchInput.tsx
+++ src/components/ui/Input/SearchInput.tsx
@@ -5,44 +5,51 @@
  * shadcn/ui: https://ui.shadcn.com/docs/components/input
  */
 
+'use client';
+
 import { SearchIcon } from '@/assets/images/icons';
 import { cn } from '@/lib/tailwind/utils';
 
 import { VariantProps } from 'class-variance-authority';
-import { FC, InputHTMLAttributes, SVGAttributes } from 'react';
+import { FC, InputHTMLAttributes, useRef } from 'react';
 
-import { Icon, iconVariants } from '..';
+import { Icon, iconVariants } from '../';
 import { Input, inputVariants } from './Input';
 
-interface ISearchInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  icon?: FC<SVGAttributes<SVGElement>>;
+interface ISearchInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'> {
   iconProps?: VariantProps<typeof iconVariants>;
 }
-
-const defaultType = 'search';
-
-const defaultIcon = SearchIcon;
 
 const defaultIconProps: VariantProps<typeof iconVariants> = {
   size: 4,
   color: 'gray',
 };
 
-export const SearchInput: FC<ISearchInputProps> = (
-  { className, type = defaultType, icon = defaultIcon, iconProps = defaultIconProps, ...props },
-  ref,
-) => {
+export const SearchInput: FC<ISearchInputProps> = ({
+  className,
+  iconProps = defaultIconProps,
+  ...props
+}) => {
   const { color: iconColor, size: iconSize } = iconProps;
+
+  // logic to focus the input when the user clicks on the container
+  const ref = useRef<HTMLInputElement>(null);
+  const focusInput = () => {
+    if (ref.current) {
+      ref.current.focus();
+    }
+  };
   return (
-    <div className={cn('flex items-center', inputVariants({ variant: 'rounded' }))}>
-      <Icon icon={icon} color={iconColor} size={iconSize} className="mr-2" />
-      <Input
-        type={type}
-        variant={'muted'}
-        className={cn('flex-1', className)}
-        ref={ref}
-        {...props}
-      />
+    <div
+      className={cn(
+        'flex items-center cursor-text',
+        inputVariants({ variant: 'rounded' }),
+        className,
+      )}
+      onClick={focusInput}
+    >
+      <Icon icon={SearchIcon} color={iconColor} size={iconSize} className="mr-2" />
+      <Input type="search" variant="muted" className={'flex-1'} {...props} ref={ref} />
     </div>
   );
 };
