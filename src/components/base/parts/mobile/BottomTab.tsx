fbc7bd70723dfc8b8b65c751e55477325bc5e6ec
kanta1207 <kan0712livepool@gmail.com>
2023-12-21 15:40:58 -0800

    fix : bottomTab z-index

--- src/components/base/parts/mobile/BottomTab.tsx
+++ src/components/base/parts/mobile/BottomTab.tsx
@@ -8,7 +8,7 @@ export const BottomTab = () => {
   const pathName = usePathname();
 
   return (
-    <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-light">
+    <div className="fixed bottom-0 left-0 z-40 w-full h-16 bg-white border-t border-gray-light">
       <div className="grid h-full max-w-lg grid-cols-3 mx-auto font-medium">
         <BottomTabIconLink name="groups" isSelected={pathName === mainRouteConfig['groups'].path} />
         <BottomTabIconLink name="foods" isSelected={pathName === mainRouteConfig['foods'].path} />
