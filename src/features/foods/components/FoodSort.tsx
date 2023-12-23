cde9293d000910e2cedc8d30345efc7aa7c6db05
kotaaaa <kota.k.1132.pda@gmail.com>
2023-12-23 10:59:45 -0800

    feat: fix icon at foodsort

--- src/features/foods/components/FoodSort.tsx
+++ src/features/foods/components/FoodSort.tsx
@@ -1,4 +1,4 @@
-import { CaretRightIcon } from '@/assets/images/icons';
+import { CaretDownIcon } from '@/assets/images/icons';
 import {
   Icon,
   Label,
@@ -49,7 +49,7 @@ export const FoodSort = () => {
         <SelectionDrawerTrigger asChild>
           <button className="flex items-center">
             {sortOptions[selectedOption as keyof typeof sortOptions]}
-            <Icon icon={CaretRightIcon} className="m-5" />
+            <Icon icon={CaretDownIcon} className="m-5" />
           </button>
         </SelectionDrawerTrigger>
         <SelectionDrawerContent>
