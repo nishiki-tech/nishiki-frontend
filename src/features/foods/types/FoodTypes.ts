53e13e795d776afc7a6e188b8bb383d907d760a9
Yusuke Ito <u.skayyyyy@gmail.com>
2023-12-25 23:49:44 -0800

    refactor: fix IdNameMapType

--- src/features/foods/types/FoodTypes.ts
+++ src/features/foods/types/FoodTypes.ts
@@ -4,4 +4,4 @@ export interface IFoodView extends IFood {
   container: string;
 }
 export type GroupIdContainersMapType = Record<IContainer['group']['id'], IContainer['id'][]>;
-export type IdNameMapType = Record<IContainer['group']['id'], IContainer['id']>;
+export type IdNameMapType = Record<string, string>;
