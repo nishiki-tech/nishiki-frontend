f6e92caeda3bee2e2c561d40755d679f326cf6ae
Shuhei Ota <shuhei.ota.so@gmail.com>
2023-12-26 19:53:23 -0800

    feat: fetch and display member list

--- src/components/page/MembersPage.tsx
+++ src/components/page/MembersPage.tsx
@@ -1,3 +1,13 @@
-export const MembersPage = () => {
-  return <>Members</>;
+import { fetchUserList } from '@/lib/api/data';
+import { IUser } from '@/types/definition';
+
+export const MembersPage = async ({ id }: { id: string }) => {
+  const users: IUser[] = await fetchUserList(id);
+  return (
+    <>
+      {users.map((user, i) => (
+        <div key={i}> {user.name} </div>
+      ))}
+    </>
+  );
 };
