eefe7bbf8531ae6f4fc46a94e484eb9a3940a34b
Shuhei Ota <shuhei.ota.so@gmail.com>
2023-12-29 00:19:13 -0800

    fix: modify UI to follow design in Figma

--- src/components/page/MembersPage.tsx
+++ src/components/page/MembersPage.tsx
@@ -11,20 +11,24 @@ export const MembersPage = async ({ id }: IMembersPageProps) => {
   const users: IUser[] = await fetchUserList(id);
   return (
     <div className="px-4">
-      <div className="flex items-center mt-[35px] mb-5">
-        <h2 className="text-xl mr-44">Members</h2>
-        <div className="w-12 h-12 flex justify-center items-center">
-          <Icon icon={MenuMeatballIcon} className="w-5 h-1" />
-        </div>
-        <div className="w-12 h-12 flex items-center justify-center">
-          <Icon icon={PlusIcon} className="w-[1.125rem] h-[1.125rem]" />
+      <div className="flex items-center justify-between mt-[35px] mb-5">
+        <h2 className="text-xl">Members</h2>
+        <div className="flex gap-0.5">
+          <div className="w-12 h-12 flex justify-center items-center">
+            <Icon icon={MenuMeatballIcon} className="w-5 h-1" />
+          </div>
+          <div className="w-12 h-12 flex justify-center items-center">
+            <Icon icon={PlusIcon} className="w-[1.125rem] h-[1.125rem]" />
+          </div>
         </div>
       </div>
       <div className="flex flex-col gap-2">
         {users.map((user, i) => (
-          <Card key={i} className="flex h-[3.75rem] items-center">
-            <Icon icon={PersonCircleIcon} color="gray" size={11} className="pl-1" />
-            <div className="ml-3">{user.name}</div>
+          <Card key={i} asChild>
+            <button className="flex gap-3 h-[3.75rem] items-center">
+              <Icon icon={PersonCircleIcon} color="gray" size={11} />
+              {user.name}
+            </button>
           </Card>
         ))}
       </div>
