2a78afb784597f76f80cbfed30d22f09241663f0
kotaaaa <kota.k.1132.pda@gmail.com>
2024-01-06 00:39:43 -0800

    feat: uninstall axios

--- src/lib/api/authApiClient.ts
+++ src/lib/api/authApiClient.ts
@@ -1,7 +1,5 @@
 // import { IContainer, IFood, IGroup, IUser } from '@/types/definition';
 
-// import axios from 'axios';
-
 // const APIDOMAIN = 'http://localhost:8080';
 
 // auth resource RESTful API client will be here
