2013d1ccf90d4671e022cd7135b2db74827af4a9
kotaaaa <kota.k.1132.pda@gmail.com>
2024-01-09 19:36:34 -0800

    feat: added first test case redirect with jest

---
+++ __tests__/app/page.test.tsx
@@ -0,0 +1,18 @@
+import Page from '@/app/page';
+
+import { redirect } from 'next/navigation';
+
+// Mocking redirect function
+jest.mock('next/navigation', () => ({
+  redirect: jest.fn(),
+}));
+
+describe('Home Component', () => {
+  it('redirects to /groups', () => {
+    // Render Home component (though it does not have a visual output)
+    Page();
+
+    // Expect redirect to have been called with '/groups'
+    expect(redirect).toHaveBeenCalledWith('/groups');
+  });
+});
