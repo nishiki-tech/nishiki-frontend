086c91db8c06ab65cd899fadd9f61c2fa7e38408
kotaaaa <kota.k.1132.pda@gmail.com>
2024-01-11 13:04:08 -0800

    feat: tests: added testcase for commonUtils.ts with typescript

--- tests/lib/api/commonUtils.test.ts
+++ tests/lib/api/commonUtils.test.ts
@@ -1,30 +1,35 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { getToken } from '@/lib/api/authTokenFetcher';
+import { getToken as originalGetToken } from '@/lib/api/authTokenFetcher';
+const getToken = originalGetToken as jest.MockedFunction<typeof originalGetToken>;
+
+// test target method
 import { request } from '@/lib/api/commonUtils';
 
 // Mock fetch and getToken
-global.fetch = jest.fn();
-jest.mock('@/lib/api/authTokenFetcher', () => ({
-  getToken: jest.fn(),
-}));
+global.fetch = jest.fn() as jest.MockedFunction<typeof fetch>;
+jest.mock('@/lib/api/authTokenFetcher');
 
 describe('request function', () => {
   const mockUrl = 'https://example.com/api/test';
   const mockMethod = 'GET';
   const mockResponseData = { data: 'test' };
-  const mockToken = { idToken: 'fake-token' };
+  const mockToken = 'fake-token';
 
   beforeEach(() => {
-    fetch.mockClear();
-    getToken.mockClear();
+    (fetch as jest.MockedFunction<typeof fetch>).mockClear();
+    (getToken as jest.MockedFunction<typeof getToken>).mockClear();
   });
 
   it('should make an API call and return data', async () => {
     getToken.mockResolvedValue(mockToken);
-    fetch.mockResolvedValue({
+    (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValue({
       ok: true,
+      status: 200,
+      statusText: 'OK',
+      headers: {
+        get: jest.fn(),
+      },
       json: () => Promise.resolve(mockResponseData),
-    });
+    } as unknown as Response);
 
     const result = await request({ url: mockUrl, method: mockMethod });
 
@@ -34,19 +39,21 @@ describe('request function', () => {
 
   it('should throw an error when the response is not ok', async () => {
     getToken.mockResolvedValue(mockToken);
-    fetch.mockResolvedValue({
+    (fetch as jest.MockedFunction<typeof fetch>).mockResolvedValue({
       ok: false,
-      status: 404,
-      json: () => Promise.resolve({}),
-    });
-
-    await expect(request({ url: mockUrl, method: mockMethod })).rejects.toThrow(
-      'HTTP error. status: 404',
-    );
+      status: 500,
+      statusText: 'API response error',
+      headers: {
+        get: jest.fn(),
+      },
+      json: () => Promise.reject(),
+    } as unknown as Response);
+    const result = request({ url: mockUrl, method: mockMethod });
+    await expect(result).rejects.toThrow('HTTP error. status: 500');
   });
 
   it('should throw an error when token is not available', async () => {
-    getToken.mockResolvedValue(null);
+    getToken.mockResolvedValue(undefined);
 
     await expect(request({ url: mockUrl, method: mockMethod })).rejects.toThrow(
       'API Unauthorized error.',
