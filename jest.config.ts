f5426bfc214b9c2b6cddf91215f06962d9ad6c53
kotaaaa <kota.k.1132.pda@gmail.com>
2024-01-11 14:11:58 -0800

    fix: modified readme, packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 /**
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
+ * https://kulshekhar.github.io/ts-jest/docs/getting-started/installation
  */
 
 /** @type {import("ts-jest/dist/types").InitialOptionsTsJest} */
@@ -11,4 +12,6 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^tests/(.*)$': '<rootDir>/tests/$1',
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 };
