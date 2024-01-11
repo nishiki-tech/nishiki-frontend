b268d0e60f24254d599f4f4570d119f369a59ba0
Yusuke Ito <u.skayyyyy@gmail.com>
2024-01-10 21:06:51 -0800

    feat: add form tag and submit handler function

--- src/features/foods/components/Form/AddDrawerContent/index.tsx
+++ src/features/foods/components/Form/AddDrawerContent/index.tsx
@@ -15,6 +15,8 @@ import {
   GroupIdNameMapType,
 } from '@/features/foods/utils/containerMapping';
 
+import { FormEvent } from 'react';
+
 import { AddDrawerBody } from './AddDrawerBody';
 
 interface IAddDrawerContentProps {
@@ -42,9 +44,27 @@ export const AddDrawerContent = ({
   /**
    * Process when the add button is clicked
    */
-  const handleAddClick = () => {
-    alert('Successfully added!');
-    setIsDrawerOpen(false);
+  // const handleAddClick = () => {
+  //   alert('Successfully added!');
+  //   setIsDrawerOpen(false);
+  // };
+
+  /**
+   * Process when the form is submitted
+   */
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const formDataObject = Object.fromEntries(formData);
+
+    const data = await fetch('/api/form', {
+      method: 'POST',
+      body: JSON.stringify(formDataObject),
+    }).then((res) => res.json());
+
+    console.log(data);
   };
 
   return (
@@ -52,22 +72,24 @@ export const AddDrawerContent = ({
       <DrawerHeader>
         <DrawerTitle>Add Food</DrawerTitle>
       </DrawerHeader>
-      <AddDrawerBody
-        groupIdContainerIdsMap={groupIdContainerIdsMap}
-        containerIdGroupIdMap={containerIdGroupIdMap}
-        containerIdNameMap={containerIdNameMap}
-        groupIdNameMap={groupIdNameMap}
-      />
-      <DrawerFooter>
-        <DrawerClose asChild>
-          <Button variant="cancel" size="sm" onClick={handleCancelClick}>
-            Cancel
+      <form onSubmit={handleSubmit}>
+        <AddDrawerBody
+          groupIdContainerIdsMap={groupIdContainerIdsMap}
+          containerIdGroupIdMap={containerIdGroupIdMap}
+          containerIdNameMap={containerIdNameMap}
+          groupIdNameMap={groupIdNameMap}
+        />
+        <DrawerFooter>
+          <DrawerClose asChild>
+            <Button variant="cancel" size="sm" onClick={handleCancelClick}>
+              Cancel
+            </Button>
+          </DrawerClose>
+          <Button type="submit" variant="primary" size="sm">
+            Add food
           </Button>
-        </DrawerClose>
-        <Button variant="primary" size="sm" onClick={handleAddClick}>
-          Add food
-        </Button>
-      </DrawerFooter>
+        </DrawerFooter>
+      </form>
     </DrawerContent>
   );
 };
