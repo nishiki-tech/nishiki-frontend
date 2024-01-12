7bd6479bb8172f365cd55459bef2cec54ab83164
kanta1207 <kan0712livepool@gmail.com>
2024-01-12 00:32:48 -0800

    refactor: refactor file name

--- src/components/page/GroupsPage.tsx
+++ src/components/page/GroupsPage.tsx
@@ -3,23 +3,13 @@ import {
   GroupCardList,
   GroupCollectionMenuButton,
 } from '@/features/groups/components';
-import { createGroup } from '@/lib/api';
 
 export const GroupsPage = () => {
-  /**
-   * Create a new group.
-   * @param {string} groupName The name of the new group.
-   * @returns The ID of the newly created group.
-   */
-  const handleCreateGroup = async (groupName: string) => {
-    'use server';
-    await createGroup({ groupName });
-  };
   return (
     <div className="pt-6 px-4 pb-2 flex flex-col gap-2">
       <div className="h-12 w-full flex items-center justify-end gap-0.5">
         <GroupCollectionMenuButton />
-        <CreateGroupButton handleCreateGroup={handleCreateGroup} />
+        <CreateGroupButton />
       </div>
       <GroupCardList />
     </div>
--- src/features/groups/components/CreateGroupButton/CreateGroupDrawerContent.tsx
+++ src/features/groups/components/CreateGroupButton/CreateGroupDrawerContent.tsx
@@ -12,7 +12,7 @@ import {
 
 import { useState } from 'react';
 
-import { createGroupAction } from './createGroupAction';
+import { createGroupAction } from './createGroupaction';
 
 export const CreateGroupDrawerContent = () => {
   const [groupName, setGroupName] = useState('');
