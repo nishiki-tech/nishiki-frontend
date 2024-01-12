72a68e1c732dd5b199d1b20862218c5a1b765519
Shuhei Ota <shuhei.ota.so@gmail.com>
2024-01-11 17:50:56 -0800

    test: add 20 li for test

--- src/components/ui/SecondaryDrawerUsageExample.tsx
+++ src/components/ui/SecondaryDrawerUsageExample.tsx
@@ -33,11 +33,31 @@ export const SecondaryDrawerUsageExample = () => {
         </Button>
       </SecondaryDrawerTrigger>
       <SecondaryDrawerContent side="bottom">
-        <SecondaryDrawerHeader>
+        <SecondaryDrawerHeader closeText="Done">
           <SecondaryDrawerTitle>Delete Container</SecondaryDrawerTitle>
         </SecondaryDrawerHeader>
         <SecondaryDrawerBody>
           <ol>
+            <li>1. Are you sure you want to delete?</li>
+            <li>2. Are you sure you want to delete?</li>
+            <li>3. Are you sure you want to delete?</li>
+            <li>4. Are you sure you want to delete?</li>
+            <li>5. Are you sure you want to delete?</li>
+            <li>6. Are you sure you want to delete?</li>
+            <li>7. Are you sure you want to delete?</li>
+            <li>8. Are you sure you want to delete?</li>
+            <li>9. Are you sure you want to delete?</li>
+            <li>10. Are you sure you want to delete?</li>
+            <li>11. Are you sure you want to delete?</li>
+            <li>12. Are you sure you want to delete?</li>
+            <li>13. Are you sure you want to delete?</li>
+            <li>14. Are you sure you want to delete?</li>
+            <li>15. Are you sure you want to delete?</li>
+            <li>16. Are you sure you want to delete?</li>
+            <li>17. Are you sure you want to delete?</li>
+            <li>18. Are you sure you want to delete?</li>
+            <li>19. Are you sure you want to delete?</li>
+            <li>20. Are you sure you want to delete?</li>
             <li>1. Are you sure you want to delete?</li>
             <li>2. Are you sure you want to delete?</li>
             <li>3. Are you sure you want to delete?</li>
