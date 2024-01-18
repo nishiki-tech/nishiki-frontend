3ded05ae90a9e356ac4001ffe5b5cf9da495b9e6
kanta1207 <99339182+kanta1207@users.noreply.github.com>
2024-01-17 16:15:33 -0800

    Fix : Missing review points on checkbox component (#191)
    * feat: implement checkbox component
    
    * refactor: remove default imports
    
    * refactor: refactor usage example
    
    * refactor: add comment to checkbox component and usage example
    
    * fix: fix checkbox comp and usage example following the PR review
    
    * fix: fix border-width using px
    
    * fix: update usage example file

--- src/components/ui/CheckboxUsageExample.tsx
+++ src/components/ui/CheckboxUsageExample.tsx
@@ -11,27 +11,27 @@ export const CheckboxUsageExample = () => {
     <div className="p-4 flex flex-col gap-4">
       {/* Example with text */}
       <Card asChild>
-        <label className="flex justify-between items-center py-2 px-4">
+        <Label htmlFor="container1" className="flex justify-between items-center py-2 px-4">
           <div className="flex items-center gap-4">
             <div className="w-11 h-11 rounded-full bg-accent flex justify-center items-center">
               <Icon icon={ContainerIcon} size={5} color="black" />
             </div>
-            <Label htmlFor="container1">Freezer</Label>
+            <span>Freezer</span>
           </div>
           <Checkbox id="container1" />
-        </label>
+        </Label>
       </Card>
       {/* Example with disabled state */}
       <Card asChild>
-        <div className="flex justify-between items-center py-2 px-4">
+        <Label htmlFor="container2" className="flex justify-between items-center py-2 px-4">
           <div className="flex items-center gap-4">
             <div className="w-11 h-11 rounded-full bg-accent flex justify-center items-center">
               <Icon icon={ContainerIcon} size={5} color="black" />
             </div>
-            <Label htmlFor="container2">Disabled</Label>
+            <span>Disabled</span>
           </div>
           <Checkbox id="container2" disabled />
-        </div>
+        </Label>
       </Card>
     </div>
   );
