287c7136a927f0d82cef86c3a6b5811023db0ac1
kanta1207 <kan0712livepool@gmail.com>
2024-01-24 13:16:13 -0800

    feat: implement create group form

--- src/features/groups/components/CreateGroupButton/CreateGroupDrawerContent.tsx
+++ src/features/groups/components/CreateGroupButton/CreateGroupDrawerContent.tsx
@@ -8,18 +8,45 @@ import {
   DrawerTitle,
   Input,
 } from '@/components/ui';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/Form';
 import { createGroup } from '@/lib/api/group/client';
-import { IGroup } from '@/types/definition';
 
-import { FormEvent, useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 export const CreateGroupDrawerContent = () => {
-  const [groupName, setGroupName] = useState<IGroup['name']>('');
+  const formSchema = z.object({
+    groupName: z.string().min(1, { message: 'Name is required' }),
+  });
 
-  // Temporary Submit function. It will be replaced when we're implementing `Form` component
-  const onSubmitCreateGroup = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      groupName: '',
+    },
+  });
+
+  /**
+   * The type of the form inputs based on the schema
+   */
+  type Inputs = z.infer<typeof formSchema>;
+
+  /**
+   * Process the form submission.
+   * @param values The form values
+   */
+  const processSubmit: SubmitHandler<Inputs> = async (values) => {
+    const { groupName } = values;
     await createGroup({ groupName });
+    form.reset();
   };
 
   return (
@@ -27,27 +54,35 @@ export const CreateGroupDrawerContent = () => {
       <DrawerHeader>
         <DrawerTitle>Create Group</DrawerTitle>
       </DrawerHeader>
-      <form onSubmit={onSubmitCreateGroup}>
-        <DrawerBody>
-          <Input
-            id="groupName"
-            name="groupName"
-            placeholder="Group Name"
-            value={groupName}
-            onChange={(e) => setGroupName(e.target.value)}
-          />
-        </DrawerBody>
-        <DrawerFooter>
-          <DrawerClose asChild>
-            <Button variant="cancel" size="md">
-              Cancel
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(processSubmit)}>
+          <DrawerBody>
+            <FormField
+              control={form.control}
+              name="groupName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel required>Group name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            ></FormField>
+          </DrawerBody>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button variant="cancel" size="md">
+                Cancel
+              </Button>
+            </DrawerClose>
+            <Button type="submit" variant="primary" size="md">
+              Create
             </Button>
-          </DrawerClose>
-          <Button type="submit" variant="primary" size="md">
-            Create
-          </Button>
-        </DrawerFooter>
-      </form>
+          </DrawerFooter>
+        </form>
+      </Form>
     </DrawerContent>
   );
 };
