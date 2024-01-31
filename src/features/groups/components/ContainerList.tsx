1df61d14f761f25660845033831b021eaf9a7ca3
Kota Kawaguchi <kota.k.1132.pda@gmail.com>
2024-02-01 06:32:26 +0900

    Feat: Apply group single page design (#197)

--- src/features/groups/components/ContainerList.tsx
+++ src/features/groups/components/ContainerList.tsx
@@ -1,23 +1,33 @@
-import { ContainerIcon } from '@/assets/images/icons';
-import { Card, Icon } from '@/components/ui';
+import { ContainerIcon, PlusIcon } from '@/assets/images/icons';
+import { Button, Card, Icon } from '@/components/ui';
 import { fetchContainerList } from '@/lib/api/container/server';
 import { IContainer } from '@/types/definition';
+
+import Link from 'next/link';
 
 export const ContainerList = async ({ id }: { id: string }) => {
   const containers: IContainer[] = await fetchContainerList(id);
   return (
     <>
-      Container
-      {/* <MenuMeatBall /> */}
-      {/* <MenuPlus /> */}
+      <div className="flex items-center justify-between mb-2 h-12">
+        <h2 className="text-xl">Container</h2>
+        <div className="flex gap-0.5">
+          <Button className="flex justify-center items-center w-12 h-12">
+            <Icon icon={PlusIcon} size={4.5} />
+          </Button>
+        </div>
+      </div>
       {containers.map((container) => (
-        <Card key={container.id}>
-          <div className="flex gap-4 items-center">
+        <Card key={container.id} asChild>
+          <Link
+            href={`/foods?group=${id}&container=${container.id}`}
+            className="flex gap-4 items-center"
+          >
             <div className="flex items-center justify-center bg-accent rounded-full w-11 h-11">
-              <Icon icon={ContainerIcon} color="black" size={5} />
+              <Icon icon={ContainerIcon} color="black" size={6} />
             </div>
             {container.name}
-          </div>
+          </Link>
         </Card>
       ))}
     </>
