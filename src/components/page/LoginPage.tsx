553d29848ead8692134da5031a30f355c1591019
kotaaaa <kota.k.1132.pda@gmail.com>
2024-02-11 14:53:29 -0800

    feat: google login with reac-ui amplify

--- src/components/page/LoginPage.tsx
+++ src/components/page/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginForm } from '@/components/ui/LoginForm';
 export const LoginPage = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="transform -translate-y-1/3 flex flex-col items-center justify-center">
+      <div className="transform flex flex-col items-center justify-center">
         <LogoVerticalPrimary className="w-32 h-26 mb-20" />
         <LoginForm />
       </div>
--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { GoogleSigninIcon } from '@/assets/images/google';
-import { Icon } from '@/components/ui';
+
+import '@aws-amplify/ui-react/styles.css';
+
 import { getToken } from '@/lib/api/common/client';
 
-import { signInWithRedirect } from 'aws-amplify/auth';
+import { Authenticator } from '@aws-amplify/ui-react';
 import { useRouter } from 'next/navigation';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export const LoginForm = () => {
   const router = useRouter();
-  const [isLoading, setLoading] = useState(true);
 
   const checkSession = useCallback(async () => {
     try {
@@ -17,42 +17,24 @@ export const LoginForm = () => {
       console.log(idToken);
       if (idToken) {
         router.push('/groups');
-      } else {
-        setLoading(false);
       }
-    } catch (err) {
-      setLoading(false);
-    }
+    } catch (err) {}
   }, [router]);
 
   useEffect(() => {
     checkSession();
   }, [checkSession]);
 
-  if (isLoading) {
-    return (
-      <button
-        disabled
-        className="bg-white border border-gray rounded flex items-center justify-center h-10 px-8"
-        onClick={() => signInWithRedirect()}
-      >
-        <div className="flex items-center justify-center h-5 mr-2 w-5">
-          <Icon icon={GoogleSigninIcon} color="white" size={4} />
-        </div>
-        <span> Login in with Google</span>
-      </button>
-    );
-  }
+  // const signInWithGoogle = async () => {
+  //   try {
+  //     await Auth.federatedSignIn({ provider: 'Google' });
+  //   } catch (error) {
+  //     console.error('Error signing in with Google:', error);
+  //   }
+  // };
 
   return (
-    <button
-      className="bg-white border border-gray rounded flex items-center justify-center h-10 px-8"
-      onClick={() => signInWithRedirect()}
-    >
-      <div className="flex items-center justify-center h-5 mr-2 w-5">
-        <Icon icon={GoogleSigninIcon} color="white" size={4} />
-      </div>
-      <span> Login in with Google</span>
-    </button>
+    // <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <Authenticator socialProviders={['google']} hideSignUp={true} />
   );
 };
