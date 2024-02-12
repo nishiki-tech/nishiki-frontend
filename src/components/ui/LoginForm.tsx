ece2680543fd28bb575c0b00ba67b897104f9589
kotaaaa <kota.k.1132.pda@gmail.com>
2024-02-11 21:57:37 -0800

    feat: rm unused code

--- src/components/ui/LoginForm.tsx
+++ src/components/ui/LoginForm.tsx
@@ -12,29 +12,15 @@ export const LoginForm = () => {
   const router = useRouter();
 
   const checkSession = useCallback(async () => {
-    try {
-      const idToken = await getToken();
-      console.log(idToken);
-      if (idToken) {
-        router.push('/groups');
-      }
-    } catch (err) {}
+    const idToken = await getToken();
+    if (idToken) {
+      router.push('/groups');
+    }
   }, [router]);
 
   useEffect(() => {
     checkSession();
   }, [checkSession]);
 
-  // const signInWithGoogle = async () => {
-  //   try {
-  //     await Auth.federatedSignIn({ provider: 'Google' });
-  //   } catch (error) {
-  //     console.error('Error signing in with Google:', error);
-  //   }
-  // };
-
-  return (
-    // <button onClick={signInWithGoogle}>Sign in with Google</button>
-    <Authenticator socialProviders={['google']} hideSignUp={true} />
-  );
+  return <Authenticator socialProviders={['google']} hideSignUp={true} />;
 };
