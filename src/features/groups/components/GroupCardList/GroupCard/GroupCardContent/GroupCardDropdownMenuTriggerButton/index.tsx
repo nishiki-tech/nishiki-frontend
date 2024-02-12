d368d213ccb4c6bd871d1dc6039e2010a085a0c3
kanta1207 <kan0712livepool@gmail.com>
2024-02-12 00:05:23 -0800

    refactor: revert groupcard dropdown menu content component

--- src/features/groups/components/GroupCardList/GroupCard/GroupCardContent/GroupCardDropdownMenuTriggerButton/index.tsx
+++ src/features/groups/components/GroupCardList/GroupCard/GroupCardContent/GroupCardDropdownMenuTriggerButton/index.tsx
@@ -4,11 +4,15 @@ import { Button, DropdownMenu, DropdownMenuTrigger, Icon } from '@/components/ui
 
 import { FC } from 'react';
 
+import { GroupCardDropdownMenuContent } from './GroupCardDropdownMenuContent';
+
 interface IGroupCardMenuButtonProps {
   handleRenameClick: () => void;
 }
 
-export const GroupCardDropdownMenuTriggerButton: FC<IGroupCardMenuButtonProps> = ({}) => {
+export const GroupCardDropdownMenuTriggerButton: FC<IGroupCardMenuButtonProps> = ({
+  handleRenameClick,
+}) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -16,7 +20,7 @@ export const GroupCardDropdownMenuTriggerButton: FC<IGroupCardMenuButtonProps> =
           <Icon icon={MenuKebabIcon} size={4.5} />
         </Button>
       </DropdownMenuTrigger>
-      {/* <GroupCardDropdownMenuContent handleRenameClick={handleRenameClick} /> */}
+      <GroupCardDropdownMenuContent handleRenameClick={handleRenameClick} />
     </DropdownMenu>
   );
 };
