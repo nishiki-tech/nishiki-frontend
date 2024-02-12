b7b09a08766ffe72b1eefde03c11447e2a483d2a
kanta1207 <kan0712livepool@gmail.com>
2024-02-12 11:35:22 -0800

    refactor: update input usage example

--- src/components/ui/Input/InputUsageExample.tsx
+++ src/components/ui/Input/InputUsageExample.tsx
@@ -3,14 +3,34 @@
  * Once we're done with the example, we can delete this file.
  */
 
+import { useRef, useState } from 'react';
+
 import { Label } from '../';
-import { Input, NumberInput, SearchInput } from './';
+import { Input, NumberInput, SearchInput, SquareTextInput } from './';
 
 export function InputUsageExample() {
+  const [value, setValue] = useState('');
+  const squareTextInputRef = useRef<HTMLInputElement>(null);
+
+  /**
+   * When cross button click is completed,
+   * set the focus back to the input,
+   */
+  const handleCrossButtonClick = () => {
+    // clear squareTextInputRef value
+    setValue('');
+    squareTextInputRef.current?.focus();
+  };
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5 py-4">
       <Input type="text" id="groupName" placeholder="Group name" />
-      <Input variant="square" type="text" id="groupName" />
+      <SquareTextInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        ref={squareTextInputRef}
+        handleCrossButtonClick={handleCrossButtonClick}
+        className="bg-white"
+      />
       <SearchInput placeholder="Search Foods" />
       <div className="w-1/2">
         <Label htmlFor="quantity">Quantity</Label>
