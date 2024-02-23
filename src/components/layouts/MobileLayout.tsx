007b43f72e659777fb1d15f906010db1d9291cb7
Nick Ito <nick.yusuke.ito@gmail.com>
2024-02-22 20:26:42 -0800

    docs: add tsdoc

--- src/components/layouts/MobileLayout.tsx
+++ src/components/layouts/MobileLayout.tsx
@@ -8,9 +8,23 @@ import { BottomMenu, Header } from '@/components/parts';
 import { FC, ReactNode } from 'react';
 
 interface IMobileLayoutProps {
+  /**
+   * The main content to display within the layout.
+   */
   children: ReactNode;
+  /**
+   * The heading text to display at the center of the header.
+   */
   heading: string;
+  /**
+   * The component to display on the left side of the header.
+   * {@link HeaderBackButton} is a common use case for this prop.
+   */
   headerLeft?: ReactNode;
+  /**
+   * The component to display on the right side of the header.
+   * {@link HeaderMenuCircleButton} is a common use case for this prop.
+   */
   headerRight?: ReactNode;
 }
 
