841fd0865939fdc28b0e5239297d6bff60d00984
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-02-24 21:34:38 -0800

    chore: delete unnecessary code

--- src/components/page/MembersPage.tsx
+++ src/components/page/MembersPage.tsx
@@ -1,18 +1,13 @@
-// import { MenuKebabIcon, PersonCircleIcon } from '@/assets/images/icons';
 import { MobileLayout } from '@/components/layouts/MobileLayout';
 import { HeaderBackButton } from '@/components/parts/Header';
-// import { Button, Card, Icon } from '@/components/ui';
 import { InviteMemberDialog } from '@/features/groups/components/InviteMemberDialog';
 import { MemberCardList } from '@/features/groups/components/MemberCardList';
-// import { fetchUserList } from '@/lib/api/user/server';
-// import { IUser } from '@/types/definition';
 
 interface IMembersPageProps {
   groupId: string;
 }
 
 export const MembersPage = async ({ groupId }: IMembersPageProps) => {
-  // const users: IUser[] = await fetchUserList(groupId);
   return (
     <MobileLayout
       heading="Members"
