8b7608c379e7aa2d5ee533fc8066c2f4d92060ee
kanta1207 <99339182+kanta1207@users.noreply.github.com>
2024-02-27 07:16:06 -0800

    Feat: implement DeleteGroupDialog (#248)
    * feat: implement deleteGroupConfirmDialog
    
    * feat: re-implement delete group dialog with new file structure
    
    * fix: out of scope, env variable name
    
    * fix: restructure to close dropdown menu on dialog close
    
    * fix: remove unnecessary asChild in DropdownMenuItem
    
    * fix: close dropdown on closing dialog

---
+++ src/features/groups/components/GroupCardList/GroupCard/GroupCardContent/GroupCardDropdownMenuTriggerButton/GroupCardDropdownMenuContent/DeleteGroupDialogTriggerButton/index.tsx
@@ -0,0 +1,43 @@
+import { DeleteIcon } from '@/assets/images/icons';
+import {
+  DialogRoot,
+  DialogTrigger,
+  DropdownMenuButton,
+  DropdownMenuButtonIcon,
+  DropdownMenuButtonText,
+  Icon,
+} from '@/components/ui';
+
+import { useState } from 'react';
+
+import { DeleteGroupDialogContent } from './DeleteGroupDialogContent';
+
+interface IGroupCardDropdownMenuDeleteButtonProps {
+  /**
+   * The function to close the dropdown menu.
+   */
+  onDropdownMenuClose: () => void;
+}
+
+export const DeleteGroupDialogTriggerButton = ({
+  onDropdownMenuClose,
+}: IGroupCardDropdownMenuDeleteButtonProps) => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  return (
+    <DialogRoot open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+      <DialogTrigger asChild>
+        <DropdownMenuButton>
+          <DropdownMenuButtonIcon>
+            <Icon icon={DeleteIcon} size={5} color="danger" />
+          </DropdownMenuButtonIcon>
+          <DropdownMenuButtonText>Delete</DropdownMenuButtonText>
+        </DropdownMenuButton>
+      </DialogTrigger>
+      <DeleteGroupDialogContent
+        onParentClose={onDropdownMenuClose}
+        onDialogClose={() => setIsDialogOpen(false)}
+      />
+    </DialogRoot>
+  );
+};
