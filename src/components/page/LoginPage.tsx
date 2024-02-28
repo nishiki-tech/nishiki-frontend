73f11b0268dc84f78bbc6f4ea02500e1e103688c
Nick Ito <nick.yusuke.ito@gmail.com>
2024-02-28 14:03:19 -0800

    feat: adjust the style and behavior of the login form

--- src/components/page/LoginPage.tsx
+++ src/components/page/LoginPage.tsx
@@ -1,12 +1,37 @@
+'use client';
+
 import { LogoVerticalPrimary } from '@/assets/images/logos';
-import { LoginForm } from '@/components/ui/LoginForm';
+import { Spinner } from '@/assets/images/ui';
+import { Icon, WhiteOverlay } from '@/components/ui';
+import { LoginForm } from '@/features/auth/components/LoginForm/LoginForm';
+import { cn } from '@/lib/tailwind/utils';
+
+import { useState } from 'react';
+
 export const LoginPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="transform flex flex-col items-center justify-center">
-        <LogoVerticalPrimary className="w-32 h-26 mb-20" />
-        <LoginForm />
+    <>
+      {/* Loading Spinner & White Overlay */}
+      {isLoading && (
+        <div
+          className={cn(
+            'z-50 absolute left-0 top-0 w-screen h-screen',
+            'flex items-center justify-center',
+          )}
+        >
+          <WhiteOverlay />
+          <Icon icon={Spinner} color="primary" size={10} className="z-50 animate-spin" />
+        </div>
+      )}
+      {/* The main content of the page */}
+      <div className="flex items-center justify-center h-screen">
+        <div className="transform flex flex-col items-center justify-center">
+          <LogoVerticalPrimary className="w-32 h-26 mb-20" />
+          <LoginForm setIsLoading={setIsLoading} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
