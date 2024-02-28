5f7c392344e8e5503e5027ca8b54ee98574be588
Nick Ito <nick.yusuke.ito@gmail.com>
2024-02-28 13:52:20 -0800

    refactor: remove unused styles in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -69,18 +69,6 @@ module.exports = {
           '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(100%)' },
         },
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
-        },
-        pulse: {
-          '0%, 100%': { transform: 'scale(1)' },
-          '50%': { transform: 'scale(1.25)' },
-        },
       },
       animation: {
         fadeIn: 'fadeIn 0.3s ease-in-out',
@@ -89,9 +77,6 @@ module.exports = {
         slideOutToBottom: 'slideOutToBottom 0.3s ease-in-out',
         slideInFromRight: 'slideInFromRight 0.3s ease-in-out',
         slideOutToRight: 'slideOutToRight 0.3s ease-in-out',
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        pulse: 'pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
