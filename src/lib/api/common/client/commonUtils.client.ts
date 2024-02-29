b08ba56920661fe8330051bd4e3ac340894cc870
Nick Ito <nick.yusuke.ito@gmail.com>
2024-02-28 18:05:31 -0800

    feat: disable toke check when using mock API

--- src/lib/api/common/client/commonUtils.client.ts
+++ src/lib/api/common/client/commonUtils.client.ts
@@ -1,4 +1,7 @@
 'use client';
+
+import { isMockApi } from '@/utils/authUtils';
+
 import { getToken } from './authTokenFetcher.client';
 
 type HttpMethod = 'GET' | 'POST' | 'DELETE' | 'PUT';
@@ -24,25 +27,31 @@ export const request = async <T>({
   method: HttpMethod;
   options?: Omit<RequestInit, 'method' | 'headers'>;
 }): Promise<T> => {
+  const requestHeaders = new Headers();
   const token = await getToken();
-  if (!token) {
+
+  /**
+   * Set the Authorization token in the header
+   * If the API is a mock API, then skip this if statement.
+   */
+  if (!isMockApi() && !token) {
     throw new Error('Authentication token is not available');
+  } else if (token) {
+    requestHeaders.set('Authorization', `Bearer ${token}`);
   }
-  // headers with Authorization
-  const headers: Record<string, string> = {
-    Authorization: `Bearer ${token}`,
-  };
-  // Request body
+
+  // Set the Content-Type to application/json if the request should have a body
   if (options?.body) {
-    headers['Content-Type'] = 'application/json';
+    requestHeaders.set('Content-Type', 'application/json');
   }
   const fetchOptions: RequestInit = {
     method: method,
-    headers: headers,
+    headers: requestHeaders,
     ...options,
   };
 
   const response = await fetch(url, fetchOptions);
+
   if (!response.ok) {
     throw new Error(`API error with status code ${response.status}: ${response.statusText}`);
   }
