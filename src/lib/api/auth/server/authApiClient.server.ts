a28437e2a3a8e1aec50633ed309866e92f6bde93
kanta1207 <kan0712livepool@gmail.com>
2024-02-29 10:39:42 -0800

    feat: implement getCurrentUserId api client

--- src/lib/api/auth/server/authApiClient.server.ts
+++ src/lib/api/auth/server/authApiClient.server.ts
@@ -1,1 +1,31 @@
-// auth resource RESTful API client will be here
+import { IUser } from '@/types/definition';
+
+import { Err, Ok, Result } from 'result-ts-type';
+
+import { request } from '../../common/server';
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
+
+/**
+ * Interface representing the API response for auth/me API endpoint.
+ * @property userId - Id of current logged in user.
+ */
+interface IGetCurrentUserResponse {
+  userId: IUser['id'];
+}
+
+/**
+ * Fetch the current logged in user's id.
+ * @returns {Promise<Result<IGetCurrentUserResponse | string>>} - Id of current logged in user.
+ */
+export const getCurrentUserId = async (): Promise<Result<IGetCurrentUserResponse, string>> => {
+  try {
+    const response = await request<IGetCurrentUserResponse>({
+      url: API_BASE_URL + '/auth/me',
+      method: 'GET',
+    });
+    return Ok(response);
+  } catch (err) {
+    return Err('API response is invalid');
+  }
+};
