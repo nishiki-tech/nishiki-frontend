ca089ce3908efa82e9ab1c2f042b4a3a8263a69a
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-03-02 20:14:28 -0800

    feat: implemenet invitation dialog in group single page

--- src/features/groups/components/MemberList.tsx
+++ src/features/groups/components/MemberList.tsx
@@ -1,19 +1,20 @@
-import { IconPersonCircle, IconPlus } from '@/assets/images/icons';
-import { Button, Icon } from '@/components/ui';
+import { IconPersonCircle } from '@/assets/images/icons';
+import { Icon } from '@/components/ui';
 import { fetchUserList } from '@/lib/api/user/server';
 import { IUser } from '@/types/definition';
 
 import Link from 'next/link';
 
+import { InviteMemberDialog } from './InviteMemberDialog';
+
 export const MemberList = async ({ id }: { id: string }) => {
   const users: IUser[] = await fetchUserList(id);
+
   return (
     <div className="mb-6">
       <div className="flex items-center justify-between mb-2 h-12">
         <h2 className="text-xl">Members</h2>
-        <Button className="flex justify-center items-center w-12 h-12">
-          <Icon icon={IconPlus} size={4.5} />
-        </Button>
+        <InviteMemberDialog groupId={id} />
       </div>
       <Link href={`/groups/${id}/members`} className="flex flex-row gap-2">
         {users.map((_, idx) => (
