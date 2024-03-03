09d0cde0e93fb5725f84dc7c0090a2c8cd3fe6b5
ShoeheyOt <shuhei.ota.so@gmail.com>
2024-03-02 20:30:43 -0800

    refactor: change the props name for readability

--- src/features/groups/components/MemberList.tsx
+++ src/features/groups/components/MemberList.tsx
@@ -1,22 +1,29 @@
 import { IconPersonCircle } from '@/assets/images/icons';
 import { Icon } from '@/components/ui';
 import { fetchUserList } from '@/lib/api/user/server';
-import { IUser } from '@/types/definition';
+import { IGroup, IUser } from '@/types/definition';
 
 import Link from 'next/link';
 
 import { InviteMemberDialog } from './InviteMemberDialog';
 
-export const MemberList = async ({ id }: { id: string }) => {
-  const users: IUser[] = await fetchUserList(id);
+interface IMemberListProps {
+  /**
+   * an identifier of a group which members belong to
+   */
+  groupId: IGroup['id'];
+}
+
+export const MemberList = async ({ groupId }: IMemberListProps) => {
+  const users: IUser[] = await fetchUserList(groupId);
 
   return (
     <div className="mb-6">
       <div className="flex items-center justify-between mb-2 h-12">
         <h2 className="text-xl">Members</h2>
-        <InviteMemberDialog groupId={id} />
+        <InviteMemberDialog groupId={groupId} />
       </div>
-      <Link href={`/groups/${id}/members`} className="flex flex-row gap-2">
+      <Link href={`/groups/${groupId}/members`} className="flex flex-row gap-2">
         {users.map((_, idx) => (
           <Icon key={idx} icon={IconPersonCircle} color="gray" size={10} />
         ))}
