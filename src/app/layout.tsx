ef1202f5c3d204e8f52b07d20ee96a9037427fcb
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-06 19:57:53 -0800

    feat: remove twitter card title for testing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,7 @@ export const metadata: Metadata = {
     ],
   },
   twitter: {
-    // card: 'summary',
     card: 'summary_large_image',
-    title: APP_CONST.NAME,
     description: APP_CONST.DESCRIPTION,
   },
 };
