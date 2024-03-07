56acb1eebdf3331b7ead26ff9deaa1746a79adf8
kanta1207 <kan0712livepool@gmail.com>
2024-03-06 23:48:48 -0800

    fix: add priority to the image

--- src/features/profile/components/UserProfile/index.tsx
+++ src/features/profile/components/UserProfile/index.tsx
@@ -31,7 +31,7 @@ export const UserProfile = ({ userId, name }: IUserProfileProps) => {
 
   return (
     <div className="flex flex-col items-center gap-4 pt-6">
-      <Image src="/images/avatar.png" width={128} height={128} alt="avatar" />
+      <Image src="/images/avatar.png" width={128} height={128} alt="avatar" priority />
       <div className="min-w-52 max-w-52">
         {isRenameUserFormOpen ? (
           <RenameUserForm
