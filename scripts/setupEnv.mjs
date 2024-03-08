ce6296f6c2afaaeb721ffe3a2821f40918adda9f
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-07 19:10:30 -0800

    ci: change .env.local to .env.development.local

--- scripts/setupEnv.mjs
+++ scripts/setupEnv.mjs
@@ -1,17 +1,26 @@
+/**
+ * This script is used to copy the .env.example file to .env.development.local
+ * if the latter does not exist. It helps contributors who start working on the project
+ * by automatically setting up the required environment variables.
+ */
+
 import { existsSync, copyFileSync } from 'fs';
 import { join } from 'path';
 
+const BASE_ENV = '.env.example';
+const TARGET_ENV = '.env.development.local';
+
 const projectRoot = process.cwd();
-const envLocalPath = join(projectRoot, '.env.local');
-const envExamplePath = join(projectRoot, '.env.example');
+const baseEnvPath = join(projectRoot, BASE_ENV);
+const targetEnvPath = join(projectRoot, TARGET_ENV);
 
 if (process.env.VERCEL) {
-  console.log('Skipping .env.local setup for non-development environment.');
+  console.log(`Skipping ${TARGET_ENV} setup on Vercel`);
 } else {
-  if (!existsSync(envLocalPath)) {
-    copyFileSync(envExamplePath, envLocalPath);
-    console.log('.env.example was copied to .env.local');
+  if (!existsSync(targetEnvPath)) {
+    copyFileSync(baseEnvPath, targetEnvPath);
+    console.log(`${BASE_ENV} was copied to ${TARGET_ENV} local`);
   } else {
-    console.log('.env.local already exists');
+    console.log(`Skipping ${TARGET_ENV} local setup as it already exists`);
   }
 }
