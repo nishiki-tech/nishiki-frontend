de4ed3ff16ea1fa7091b002d89ffdcae17065020
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-07 18:41:20 -0800

    ci: change the env handling

--- scripts/setupEnv.mjs
+++ scripts/setupEnv.mjs
@@ -5,13 +5,13 @@ const projectRoot = process.cwd();
 const envLocalPath = join(projectRoot, '.env.local');
 const envExamplePath = join(projectRoot, '.env.example');
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+if (process.env.VERCEL) {
+  console.log('Skipping .env.local setup for non-development environment.');
+} else {
   if (!existsSync(envLocalPath)) {
     copyFileSync(envExamplePath, envLocalPath);
     console.log('.env.example was copied to .env.local');
   } else {
     console.log('.env.local already exists');
   }
-} else {
-  console.log('Skipping .env.local setup for non-development environment.');
 }
