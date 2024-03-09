66a8aa323c89310a421f6002d5c1eb183a0bba1a
Nick (Yusuke) Ito <nick.yusuke.ito@gmail.com>
2024-03-09 07:30:09 -0800

    Feature: Set up App icons & Web Manifest (#303)
    * refactor: remove unnecessary image files
    
    * feat: add favicon
    
    * feat: shortcut and apple icons
    
    * refactor: remove stale images
    
    * feat: add favicon and apple icon
    
    * feat: add square logo
    
    * fix: change icons locations to make them available in android
    
    * feat: add app icons
    
    * feat: create manifest.json
    
    * chore: remove shortcut icons
    
    * feat: add prefer_related_applications to manifest
    
    * feat: make the icons maskable to fill touch icon on Android
    
    * feat: add any to icons
    
    * feat: add theme color meta data
    
    * fix: fix the theme color meta data definition
    
    * feat: add icons for `any` purpose
    
    * feat: resize app screenshots and apply them to manifest
    
    * feat: adjust shortcut names
    
    * fix: the theme-color of the meta data
    
    * feat: add ogp image
    
    * feat: configure metadata
    
    * feat: add twitter metadata
    
    * feat: add PNG og image
    
    * feat: replace the SVP OG image for twitter with PNG
    
    * feat: add siteName
    
    * feat: replace the SVG OG image with PNG
    
    * feat: replace the apple touch icon and rename it
    
    * feat: replace images
    
    * refactor: refactor directory structure for public images
    
    * chore: remove title for testing
    
    * docs: adjust comments
    
    * feat: create appConst file and replace values
    
    * feat: change the twitter card type to summary large image
    
    * fix: icon paths
    
    * feat: replace the SVG apple touch icon with PNG
    
    * feat: remove twitter card title for testing
    
    * feat: remove svg apple touch icon, which is unused
    
    * feat: add type for favicon
    
    * feat: add ico favicon
    
    * chore: remove unused SVG og image
    
    * chore: remove unused favicon.svg
    
    * fix: favicon path
    
    * feat: add title to twitter card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,55 @@
 import '@/styles/globals.css';
 
+import { APP_CONST } from '@/const/appConst';
 import { fontOutfit } from '@/const/fonts';
-import { siteConfig } from '@/const/site/siteConfig';
 import { cn } from '@/lib/tailwind/utils';
 
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 import { ReactNode } from 'react';
 
+/**
+ * This object configures the metadata and generate meta tags for the app.
+ * @see {@link https://nextjs.org/docs/app/api-reference/functions/generate-metadata}
+ */
 export const metadata: Metadata = {
   title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
+    default: APP_CONST.NAME,
+    template: `%s | ${APP_CONST.NAME}`,
   },
-  description: siteConfig.description,
+  description: APP_CONST.DESCRIPTION,
   icons: {
-    // TODO add actual icons
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
+    icon: '/images/icons/favicon.ico?v=0',
+    apple: '/images/icons/apple-touch-icon.png?v=0',
   },
+  openGraph: {
+    siteName: APP_CONST.NAME,
+    description: APP_CONST.DESCRIPTION,
+    type: 'website',
+    url: APP_CONST.BASE_URL,
+    images: [
+      {
+        url: `${APP_CONST.BASE_URL}/images/og.png?v=0`,
+        width: 1200,
+        height: 630,
+        alt: APP_CONST.NAME,
+      },
+    ],
+  },
+  twitter: {
+    title: APP_CONST.NAME,
+    card: 'summary_large_image',
+    description: APP_CONST.DESCRIPTION,
+  },
+};
+
+/**
+ * This object configures the viewport and generate meta tags for the app.s
+ * @see {@link https://nextjs.org/docs/app/api-reference/functions/generate-viewport}
+ */
+export const viewport: Viewport = {
+  themeColor: APP_CONST.THEME_COLOR, // --color-primary
 };
 
 interface IRootLayoutProps {
