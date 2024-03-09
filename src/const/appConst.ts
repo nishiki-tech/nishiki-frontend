7226f235398ad2affe41cb3e7de8f91dcefe5603
Nick Ito <nick.yusuke.ito@gmail.com>
2024-03-09 14:49:01 -0800

    fix: add metadataBase

--- src/const/appConst.ts
+++ src/const/appConst.ts
@@ -2,7 +2,7 @@ export const APP_CONST = {
   NAME: 'Nishiki',
   DESCRIPTION:
     'Nishiki is an app for tracking and sharing food inventories within groups for better pantry management.',
-  BASE_URL: process.env.NEXT_PUBLIC_CLIENT_BASE_URL,
+  BASE_URL: process.env.NEXT_PUBLIC_CLIENT_BASE_URL || 'http://localhost:3000',
   THEME_COLOR: '#6ab3ab',
   BG_COLOR: '#abd4cf',
 } as const;
