6d16656381d478f45527ec9443d209d960b8a44d
kanta1207 <kan0712livepool@gmail.com>
2024-03-12 11:55:43 -0700

    test: test remove user action

--- src/features/profile/lib/actions.test.ts
+++ src/features/profile/lib/actions.test.ts
@@ -1,8 +1,8 @@
-import { putUpdateUser } from '@/lib/api/user/client';
+import { deleteUser, putUpdateUser } from '@/lib/api/user/client';
 
 import { Err, Ok } from 'result-ts-type';
 
-import { renameUser } from './actions';
+import { removeUser, renameUser } from './actions';
 
 jest.mock('@/lib/api/user/client', () => ({
   putUpdateUser: jest.fn(),
@@ -21,7 +21,7 @@ describe('Profile page actions', () => {
       name: 'New Name',
     };
 
-    it('should rename a user successfully   ', async () => {
+    it('should rename a user successfully', async () => {
       // Arrange
       (putUpdateUser as jest.Mock).mockResolvedValue(Ok(undefined));
 
@@ -58,6 +58,50 @@ describe('Profile page actions', () => {
 
       // Act
       const result = await renameUser(mockUserId, mockInputs);
+
+      // Assert
+      expect(result.err).toBeTruthy();
+      expect(result.unwrapError()).toEqual(mockErrorMessage);
+    });
+  });
+  describe('removeUser', () => {
+    // Arrange common mock data
+    const mockUserId = 'a3kdifut-a520-c2cb-1be7-d90710691861';
+
+    it('should remove a user successfully', async () => {
+      // Arrange
+      (deleteUser as jest.Mock).mockResolvedValue(Ok(undefined));
+
+      // Act
+      const result = await removeUser(mockUserId);
+
+      // Assert
+      expect(result.ok).toBeTruthy();
+      expect(result.unwrap()).toEqual(undefined);
+      expect(deleteUser).toHaveBeenCalled();
+    });
+
+    it('should return an error if validation fails', async () => {
+      // Arrange
+      const mockErrorMessage = 'Validation failed';
+
+      // Act
+      const result = await removeUser('');
+
+      // Assert
+      expect(result.err).toBeTruthy();
+      expect(result.unwrapError()).toEqual(mockErrorMessage);
+      expect(deleteUser).not.toHaveBeenCalled();
+    });
+
+    it('should return an error if API request fails', async () => {
+      // Arrange
+      // Mock the API request to simulate failure
+      const mockErrorMessage = 'API request failed';
+      (deleteUser as jest.Mock).mockResolvedValue(Err(mockErrorMessage));
+
+      // Act
+      const result = await removeUser(mockUserId);
 
       // Assert
       expect(result.err).toBeTruthy();
